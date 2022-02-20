83a9d0a29654d031c5a0a51bd1e7f0a5e72c2776
yakov cohen <yakovc1431@gmail.com>
2022-02-20 15:48:56 +0200

    # Add Gif loader

--- front/src/Components/Loading.tsx
+++ front/src/Components/Loading.tsx
@@ -1,11 +1,7 @@
+import loader from './../Data/loader-gif.gif';
+
 const Loading = () => {
-  return (
-    <img
-      src="https://i.pinimg.com/originals/54/98/88/549888805ac40cb67a3bb7ab6a4024ea.gif"
-      alt="dictionary"
-      className="loading-gif"
-    ></img>
-  );
+  return <img src={loader} alt="dictionary" className="loading-gif"></img>;
 };
 
 export default Loading;
