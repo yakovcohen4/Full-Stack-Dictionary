3d5b8a119d53d12003502706a634c4afe251c21f
yakov cohen <yakovc1431@gmail.com>
2022-02-22 17:39:11 +0200

    # Change loading path

--- front/src/Components/Loading.tsx
+++ front/src/Components/Loading.tsx
@@ -1,4 +1,4 @@
-import loader from '../Data/loader-gif-no-background.gif';
+import loader from './../Data/loader-gif-no-background.gif';
 
 const Loading = () => {
   return <img src={loader} alt="loading-gif" className="loading-gif"></img>;
