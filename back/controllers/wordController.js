8c7e0bf716b788dbd3fd60eaa7651ef56e989464
yakov cohen <yakovc1431@gmail.com>
2022-02-22 23:45:09 +0200

    # Send error to error handler middle ware

--- back/controllers/wordController.js
+++ back/controllers/wordController.js
@@ -5,7 +5,7 @@ AWS.config.update({ region: REGION }); // for the test
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 // Search by word
-exports.findWord = async (req, res) => {
+exports.findWord = async (req, res, next) => {
   const { word } = req.params;
 
   const params = {
@@ -17,10 +17,18 @@ exports.findWord = async (req, res) => {
   };
 
   try {
+    // Validation - word is not word in english (A-Z)
+    const wordValid = /^[a-z]+$/gi.test(word);
+    if (!wordValid) {
+      throw { status: 400, message: 'not word in English' };
+    }
     const result = await dynamoDB.query(params).promise();
+    // The search did not find a word
+    if (result.Items.length === 0) {
+      throw { status: 404, message: 'no result for this word' };
+    }
     res.send(result);
   } catch (err) {
-    console.log(err);
-    res.send(err);
+    next(err);
   }
 };
