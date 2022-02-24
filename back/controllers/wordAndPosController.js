80c1833fc8d805e35caa8c80a0c538f6bb2aa68c
yakov cohen <yakovc1431@gmail.com>
2022-02-24 14:04:51 +0200

    # Change search word with POS - error throw

--- back/controllers/wordAndPosController.js
+++ back/controllers/wordAndPosController.js
@@ -1,11 +1,11 @@
-const { TABLE_NAME, REGION } = require('../utils/constants');
+const { TABLE_NAME, REGION, partOfSpeechList } = require('../utils/constants');
 
 const AWS = require('aws-sdk');
 AWS.config.update({ region: REGION }); // for the test
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 // Search by Part Of Speech and word
-exports.findWordAndPos = async (req, res) => {
+exports.findWordAndPos = async (req, res, next) => {
   const { word, partOfSpeech } = req.params;
 
   const params = {
@@ -18,10 +18,25 @@ exports.findWordAndPos = async (req, res) => {
   };
 
   try {
+    // Validation - word is not word in english (A-Z)
+    const wordValid = /^[a-z]+$/gi.test(word);
+    if (!wordValid) {
+      throw { status: 400, message: 'not word in English' };
+    }
+    // check if part of speech is in the list
+    if (!partOfSpeechList.includes(partOfSpeech)) {
+      throw { status: 400, message: 'not part of speech in English' };
+    }
     const result = await dynamoDB.query(params).promise();
+    // The search did not find a word
+    if (result.Items.length === 0) {
+      throw {
+        status: 404,
+        message: 'no result for this word & part of speech',
+      };
+    }
     res.send(result);
   } catch (err) {
-    console.log(err);
-    res.send(err);
+    next(err);
   }
 };
