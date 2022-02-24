2946f051fdf39a45c8227488f4b0f2887865a939
yakov cohen <yakovc1431@gmail.com>
2022-02-24 12:57:47 +0200

    # Add throw error to func and pos-list

--- back/controllers/partOfSpeechController.js
+++ back/controllers/partOfSpeechController.js
@@ -1,11 +1,11 @@
-const { TABLE_NAME, REGION } = require('../utils/constants');
+const { TABLE_NAME, REGION, partOfSpeechList } = require('../utils/constants');
 
 const AWS = require('aws-sdk');
 AWS.config.update({ region: REGION }); // for the test
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 // Search by Part Of Speech random word
-exports.partOfSpeech = async (req, res) => {
+exports.partOfSpeech = async (req, res, next) => {
   const { part } = req.params;
   const { letter } = req.query;
 
@@ -23,14 +23,17 @@ exports.partOfSpeech = async (req, res) => {
   };
 
   try {
+    // check if part of speech is in the list
+    if (!partOfSpeechList.includes(part)) {
+      throw { status: 400, message: 'not part of speech in English' };
+    }
     const result = await dynamoDB.scan(params).promise();
     if (result.Items.length === 0) {
-      return res.send('no result for part of speak');
+      throw { status: 404, message: 'no result for this part of speech' };
     }
     const randomNum = Math.floor(Math.random() * result.Items.length); // random number
     res.send(result.Items[randomNum]);
   } catch (err) {
-    console.log(err);
-    res.send(err);
+    next(err);
   }
 };
