b51988bc578c46770f13a0244dfe38c0c8933ea8
yakov cohen <yakovc1431@gmail.com>
2022-02-24 12:58:48 +0200

    # Add errors to error handler

--- back/middleWare/errorHandlerMiddleWare.js
+++ back/middleWare/errorHandlerMiddleWare.js
@@ -1,9 +1,23 @@
 function errorHandlerMiddleware(err, req, res, next) {
+  // word search error
   if (err.status === 400 && err.message === 'not word in English') {
     return res.status(400).send({ error: err.message });
   } else if (err.status === 404 && err.message === 'no result for this word') {
     return res.status(404).json({ error: err.message });
   }
+  // part of speech search error
+  else if (
+    err.status === 400 &&
+    err.message === 'not part of speech in English'
+  ) {
+    return res.status(400).json({ error: err.message });
+  } else if (
+    err.status === 404 &&
+    err.message === 'no result for this part of speech'
+  ) {
+    return res.status(404).json({ error: err.message });
+  }
+  // other error
   if (!err.status) {
     return res.status(500).send({ error: 'Internal server error' });
   }
