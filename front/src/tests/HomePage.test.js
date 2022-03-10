92e68480eedbe9844ebbec8d9899b9a80ef984d2
yakov cohen <yakovc1431@gmail.com>
2022-03-10 16:13:15 +0200

    # Create unit test for Home Page

---
+++ front/src/tests/HomePage.test.js
@@ -0,0 +1,21 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import HomePage from '../Components/HomePage';
+
+test('renders home page and test the header', () => {
+  render(<HomePage />);
+  const headerElement = screen.getByText(/home page/i);
+  expect(headerElement).toBeInTheDocument();
+});
+
+test('renders home page and test the sub title', () => {
+  render(<HomePage />);
+  const subTitleElement = screen.getByText(/welcome to my dictionary/i);
+  expect(subTitleElement).toBeInTheDocument();
+});
+
+test('renders home page and test the explanation to the user', () => {
+  render(<HomePage />);
+  const listOfLiElements = screen.getAllByRole('listitem');
+  expect(listOfLiElements.length).toBe(5);
+});
