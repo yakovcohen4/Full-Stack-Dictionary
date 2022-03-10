d721c2c1fe7e919ff67318e13fa463b6c4f3d0ff
yakov cohen <yakovc1431@gmail.com>
2022-03-10 17:32:32 +0200

    # Add describe to tests

--- front/src/tests/Loader.test.js
+++ front/src/tests/Loader.test.js
@@ -4,13 +4,15 @@ import { render, screen } from '@testing-library/react';
 import loader from './../Data/loader-gif-no-background.gif';
 import Loading from '../Components/Loading';
 
-test('renders Loading -> test the image alt', () => {
-  render(<Loading />);
-  const imageAlt = screen.getByAltText(/loading-gif/i);
-  expect(imageAlt).toBeInTheDocument();
-});
-test('renders Loading -> test the image src', () => {
-  render(<Loading />);
-  const image = screen.getByAltText(/loading-gif/i);
-  expect(image.src).toContain(loader);
+describe('<Loading />', () => {
+  test('renders Loading -> test the image alt', () => {
+    render(<Loading />);
+    const imageAlt = screen.getByAltText(/loading-gif/i);
+    expect(imageAlt).toBeInTheDocument();
+  });
+  test('renders Loading -> test the image src', () => {
+    render(<Loading />);
+    const image = screen.getByAltText(/loading-gif/i);
+    expect(image.src).toContain(loader);
+  });
 });
